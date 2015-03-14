9570008d89e6fea2d48e270a9bb1c5cc266193e6
Nashville Software School <nss@Nashvilles-MacBook-Pro.local>
2015-03-14 10:53:07 -0500

    newcommit

--- week7/jquery/js/dom-events.js
+++ week7/jquery/js/dom-events.js
@@ -3,3 +3,39 @@ var hiddenDiv = document.getElementById("hidden");
 
 
 
+/*
+targetDiv.onmouseover=function(){
+
+targetDiv.classList.remove('red');
+targetDiv.classList.add('blue');
+}
+*/
+
+/*targetDiv.onmouseover = function () {
+	targetDiv.classList.toggle('red');
+} */
+
+/*targetDiv.onmouseover = function () {
+	targetDiv.classList.remove('red');
+}
+targetDiv.onmouseout = function () {
+	targetDiv.classList.add('red');
+}*/
+
+var peopleToDisplay = [];
+peopleToDisplay.push("Robert Johnson");
+peopleToDisplay.push("John Lee Hooker");
+peopleToDisplay.push("T-Bone Walker");
+peopleToDisplay.push("Muddy Waters");
+peopleToDisplay.push("Big Mama Thornton");
+peopleToDisplay.push("Robert Cray");
+
+targetDiv.onclick = function () {
+	hiddenDiv.classList.toggle('hide');
+	hiddenDiv.innerHTML = peopleToDisplay.join("<br>")
+	alert ("Hello World");
+}
+
+targetDiv.onmouseout = function () {
+	hiddenDiv.classList.add('hide');
+}
